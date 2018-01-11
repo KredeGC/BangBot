0e9b8212fd27d4160f703198b4b02c5ac33f99e9
The Fern <krede2000@gmail.com>
2018-01-11 10:00:58 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -28,9 +28,7 @@ client.on('message', message => {
 	if (command == "meme") {
 		message.channel.send("Finder en dank meme...");
 		message.channel.send('file.jpg', {
-			files: [
-				["http://thefern.netau.net/api/meme/generator?meme=thot&top=begone&bottom=thot", "thot.jpg"]
-			]
+			files: ["http://thefern.netau.net/api/meme/generator?meme=thot&top=begone&bottom=thot.jpg"]
 		});
 	}
 });
