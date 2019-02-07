109aff75d12e1aec9b8f84d5e79af42fd303bcf2
The Fern <krede2000@gmail.com>
2019-02-07 16:46:08 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -223,8 +223,8 @@ function begoneAFK(user) {
 
 client.on('ready', () => {
     console.log('Bang bang into the room!');
-    client.user.setGame('-help');
-	// client.user.setPresence({ game: { name: '-help', type: 0 } });
+    // client.user.setGame('-help');
+	client.user.setPresence({ game: { name: '-help', type: 0 } });
 });
 
 /*client.on('messageReactionAdd', (react, user) => {
